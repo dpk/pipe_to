bb26a238686059ddf515b5543f2da0a7451ecd71
David Kendal <dpkendal@dpk.org.uk>
2012-01-11 21:23:48 +0000

    add .edit and .edit! for editing strings with your text editor. mainly for irb obvs

--- lib/pipe_to.rb
+++ lib/pipe_to.rb
@@ -13,4 +13,28 @@ class String
   def | cmd
     pipe_to "#{cmd}"
   end
+  
+  def edit editor=nil
+    editor = ENV['EDITOR'] if not editor
+    path = "/tmp/rbedit-#{uniqstr 4}"
+    File.open path, "w" do |f|
+      f.print self
+    end
+    system "#{editor} #{path}"
+    File.read path
+  end
+  
+  def edit! editor=nil
+    s = edit editor
+    self[0..-1] = s
+  end
+  
+  private
+  def uniqstr len
+    s = ""
+    len.times do
+      s << (97 + (rand 26)).chr
+    end
+    s
+  end
 end
